18d2cf0c1f2044402d1602646608779929e1d24d
SergeiRing <sereshka0967@gmail.com>
2019-04-15 12:44:21 +0300

    update gui

--- source/View.java
+++ source/View.java
@@ -1,65 +1,154 @@
+import java.awt.*;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
-import java.awt.Color;
-import java.awt.Panel;
-import java.awt.List;
-import java.awt.Choice;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.border.LineBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
 import java.io.File;
 
 public class View extends JFrame {
 
-    private JPanel contentPane;
-    public File maps_folder = new File("maps");
-    String[] ComboArray;
+	private JPanel contentPane;
+	public JButton[] btns = new JButton[30];
+	public int disposeY, disposeX;
+	public File maps_folder = new File("maps");
+
+	public void btnClick(JButton btn){
+		Main.CreateGameWindow(btn.getText());
+	}
 
 	public View() {
-        String[] maps = maps_folder.list();
+		String[] maps = maps_folder.list();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setUndecorated(true);
+		setUndecorated(true);
+		setResizable(false);
 		setBounds(0, 0, 1920, 1080);
 		contentPane = new JPanel();
-		contentPane.setForeground(Color.DARK_GRAY);
-		contentPane.setBackground(new Color(69,75,83));
+		contentPane.setBackground(new Color(35,35,35));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-        
-        JComboBox comboBox = new JComboBox();
-        ComboArray = new String[maps.length];
-        for( int i = 0; i < maps.length; i++ ){
-            ComboArray[i] = maps[i].replaceAll(".txt", "");
-        }
-		comboBox.setModel(new DefaultComboBoxModel(ComboArray));
-		comboBox.setName("");
-		comboBox.setBounds(856, 409, 279, 49);
-		contentPane.add(comboBox);        
 
-		JButton btnNewButton = new JButton("Play");
-		btnNewButton.setBackground(Color.LIGHT_GRAY);
-		btnNewButton.setForeground(Color.BLACK);
-		btnNewButton.setBorderPainted(false);
-		btnNewButton.setFont(new Font("Bauhaus 93", Font.PLAIN, 44));
-        btnNewButton.setBounds(856, 505, 279, 59);
-        btnNewButton.addActionListener(new ActionListener() { 
-            public void actionPerformed(ActionEvent e) { 
-              Main.CreateGameWindow(comboBox.getSelectedItem().toString());
-            } 
-          } );
-		contentPane.add(btnNewButton);
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setFont(new Font("LetterOMatic!", Font.PLAIN, 20));
+		lblNewLabel.setForeground(Color.white);
+		lblNewLabel.setBounds(21, 2, 141, 24);
 		
-		JLabel lblMaps = new JLabel("maps");
-		lblMaps.setFont(new Font("Bauhaus 93", Font.PLAIN, 45));
-		lblMaps.setBounds(943, 356, 103, 42);
-		contentPane.add(lblMaps);
+		JPanel panel = new JPanel();
+		panel.setBounds(448, 244, 346, 632);
+		panel.setBackground(new Color(41,44,43));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JSlider slider = new JSlider();
+		slider.setBackground(new Color(41,44,43));
+		slider.setBounds(265, 25, 200, 23);
+		slider.setForeground(Color.WHITE);
+		
+		JLabel lblMusic = new JLabel("Music");
+		lblMusic.setForeground(Color.WHITE);
+		lblMusic.setFont(new Font("LetterOMatic!", Font.PLAIN, 20));
+		lblMusic.setBounds(26, 25, 77, 23);
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBounds(834, 244, 696, 624);
+        contentPane.add(scrollPane);
+		
+        JPanel panel_1 = new JPanel();
+        scrollPane.setViewportView(panel_1);
+        panel_1.setBackground(new Color(41,44,43));
+        panel_1.setPreferredSize(new Dimension(696, 620));
+        panel_1.setLayout(null);
+ 
+		JButton btnNewButton = new JButton("play");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				panel_1.removeAll();
+				panel_1.setPreferredSize(new Dimension(696, 10000));
+				disposeX=0;
+				disposeY=0;
+				for( int i = 0; i<maps.length; i++ ) {
+					btns[i] = new JButton();
+					btns[i].setBorder(new LineBorder(Color.WHITE, 2, true));
+					btns[i].setFont(new Font("LetterOMatic!", Font.PLAIN, 30));
+					btns[i].setBackground(new Color(41,44,43));
+					btns[i].setBounds(15+disposeX, 27+disposeY, 151, 119);
+					btns[i].setPreferredSize(new Dimension(151, 119));
+					btns[i].setLabel(maps[i].replaceAll(".txt", ""));
+					btns[i].setForeground(Color.WHITE);
+					btns[i].setFont(new Font("LetterOMatic!", Font.PLAIN, 20));
+					btns[i].addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							btnClick((JButton) e.getSource());
+						}
+					});
+					disposeX+=170;
+					if( (i+1)%4==0 ) {
+						disposeY+=130;
+						disposeX=0;
+					}
+					
+					panel_1.add(btns[i]);
+				}
+				scrollPane.revalidate();
+				
+				panel_1.repaint();		     	        
+			}
+		});
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(new Color(41,44,43));
+		btnNewButton.setFont(new Font("LetterOMatic!", Font.PLAIN, 30));
+		btnNewButton.setBounds(10, 26, 326, 50);
+		btnNewButton.setBorder(new LineBorder(new Color(255, 255, 255), 2, true));
+		panel.add(btnNewButton);
+		
+		JButton btnSettings = new JButton("settings");
+		btnSettings.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel_1.removeAll();
+				panel_1.setPreferredSize(new Dimension(696, 620));
+				panel_1.add(slider);
+				panel_1.add(lblMusic);
+				panel_1.repaint();
+				scrollPane.revalidate();
+			}
+		});
+		btnSettings.setForeground(Color.WHITE);
+		btnSettings.setFont(new Font("LetterOMatic!", Font.PLAIN, 30));
+		btnSettings.setBorder(new LineBorder(new Color(255, 255, 255), 2, true));
+		btnSettings.setBackground(new Color(41, 44, 43));
+		btnSettings.setBounds(10, 100, 326, 50);
+		panel.add(btnSettings);
+		
+		JButton btnDevelopers = new JButton("developers");
+		btnDevelopers.setForeground(Color.WHITE);
+		btnDevelopers.setFont(new Font("LetterOMatic!", Font.PLAIN, 30));
+		btnDevelopers.setBorder(new LineBorder(new Color(255, 255, 255), 2, true));
+		btnDevelopers.setBackground(new Color(41, 44, 43));
+		btnDevelopers.setBounds(10, 550, 326, 50);
+		panel.add(btnDevelopers);
+		
+		JLabel lblAlpha = new JLabel("alpha 1.0");
+		lblAlpha.setForeground(Color.LIGHT_GRAY);
+		lblAlpha.setBackground(Color.LIGHT_GRAY);
+		lblAlpha.setFont(new Font("LetterOMatic!", Font.PLAIN, 15));
+		lblAlpha.setBounds(10, 11, 105, 18);
+		contentPane.add(lblAlpha);
+		
 	}
 }
